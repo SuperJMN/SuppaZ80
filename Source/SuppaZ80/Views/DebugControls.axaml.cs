9ec1c17a504deefc0dfb80d3620b2c05dd4b4ef0
José Manuel Nieto <superjmn@outlook.com>
2022-11-14 16:01:32 +0100

    Improve look&feel

---
+++ Source/SuppaZ80/Views/DebugControls.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SuppaZ80.Views
+{
+    public partial class DebugControls : UserControl
+    {
+        public DebugControls()
+        {
+            InitializeComponent();
+        }
+    }
+}
